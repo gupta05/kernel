9ce576e9a192bb897b6ede68706172c1077236b0
Bjorn Andersson <bjorn.andersson@sonymobile.com>
2014-02-03 17:39:02 -0800

    Fusion3 CDB

--- arch/arm/mach-msm/board-dt.c
+++ arch/arm/mach-msm/board-dt.c
@@ -17,12 +17,15 @@
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 
+#include <asm/setup.h>
+
 #include "common.h"
 
 static const char * const msm_dt_match[] __initconst = {
 	"qcom,msm8660-fluid",
 	"qcom,msm8660-surf",
 	"qcom,msm8960-cdp",
+	"qcom,msm8960",
 	NULL
 };
 
@@ -31,9 +34,24 @@ static const char * const apq8074_dt_match[] __initconst = {
 	NULL
 };
 
+static void msm_dt_init_meminfo(void)
+{
+	struct membank *bank;
+	int i;
+
+	for (i = 0; i < meminfo.nr_banks; i++) {
+		bank = &meminfo.bank[i];
+		if (bank->start == 0x80200000) {
+			bank->start -= 0x200000;
+			bank->size += 0x200000;
+		}
+	}
+}
+
 DT_MACHINE_START(MSM_DT, "Qualcomm MSM (Flattened Device Tree)")
 	.smp = smp_ops(msm_smp_ops),
 	.dt_compat = msm_dt_match,
+	.init_meminfo = msm_dt_init_meminfo,
 MACHINE_END
 
 DT_MACHINE_START(APQ_DT, "Qualcomm MSM (Flattened Device Tree)")
