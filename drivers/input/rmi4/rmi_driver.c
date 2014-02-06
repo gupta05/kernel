cade8d8be0d0c5047967512887595f0184ed8a5c
Courtney Cavin <courtney.cavin@sonymobile.com>
2014-02-05 17:13:40 -0800

    Input: synaptics-rmi4 - correct RMI4 spec url
    Signed-off-by: Courtney Cavin <courtney.cavin@sonymobile.com>
    Acked-by: Christopher Heiny <cheiny@synaptics.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/rmi4/rmi_driver.c
+++ drivers/input/rmi4/rmi_driver.c
@@ -7,7 +7,7 @@
  * The RMI4 specification can be found here (URL split for line length):
  *
  * http://www.synaptics.com/sites/default/files/
- *      511-000136-01-Rev-E-RMI4%20Intrfacing%20Guide.pdf
+ *      511-000136-01-Rev-E-RMI4-Interfacing-Guide.pdf
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published by
