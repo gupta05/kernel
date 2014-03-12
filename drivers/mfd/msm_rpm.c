3160f2471cc63165a3fe2241dc191b0a7b467ad5
Bjorn Andersson <bjorn.andersson@sonymobile.com>
2014-03-12 14:06:19 -0700

    mfd: msm_rpm: Add all 8064 and 8960 resources
    Signed-off-by: Bjorn Andersson <bjorn.andersson@sonymobile.com>

--- drivers/mfd/msm_rpm.c
+++ drivers/mfd/msm_rpm.c
@@ -19,7 +19,8 @@
 
 #include <linux/mfd/msm_rpm.h>
 
-#define MSM_RPM_MAX_RESOURCES 125
+#include "msm_rpm-8064.h"
+#include "msm_rpm-8960.h"
 
 struct msm_rpm_resource;
 
@@ -53,23 +54,220 @@ struct msm_rpm {
 #define MSM_ACK_REG(rpm, i) ((rpm)->ack_regs + (i) * 4)
 
 struct msm_rpm_resource {
-	unsigned ctrl_id;
 	unsigned target_id;
 	unsigned status_id;
-	unsigned count;
-	unsigned sel;
+	unsigned select_id;
+	unsigned size;
+};
+
+#define MSM_RPM_VERSION		0
+#define MSM_RPM_REQ_CTX		3
+#define MSM_RPM_REQ_SEL		11
+#define MSM_RPM_REQ_SEL_COUNT	7
+#define MSM_RPM_ACK_CTX		15
+#define MSM_RPM_ACK_SEL		23
+#define MSM_RPM_ACK_SEL_COUNT	7
+
+/*
+ * APQ8064 data
+ */
+#define APQ8064_RESOURCE(id, size)		\
+	[MSM_RPM_ ## id] = {			\
+		MSM_RPM_8064_ID_ ## id,		\
+		MSM_RPM_8064_STATUS_ID_ ## id,	\
+		MSM_RPM_8064_SEL_ ## id,	\
+		size				\
+	}
+
+static const struct msm_rpm_resource apq8064_rpm_resource_table[] = {
+	APQ8064_RESOURCE(CXO_CLK, 1),
+	APQ8064_RESOURCE(PXO_CLK, 1),
+	APQ8064_RESOURCE(APPS_FABRIC_CLK, 1),
+	APQ8064_RESOURCE(SYSTEM_FABRIC_CLK, 1),
+	APQ8064_RESOURCE(MM_FABRIC_CLK, 1),
+	APQ8064_RESOURCE(DAYTONA_FABRIC_CLK, 1),
+	APQ8064_RESOURCE(SFPB_CLK, 1),
+	APQ8064_RESOURCE(CFPB_CLK, 1),
+	APQ8064_RESOURCE(MMFPB_CLK, 1),
+	APQ8064_RESOURCE(EBI1_CLK, 1),
+
+	APQ8064_RESOURCE(APPS_FABRIC_CFG_HALT, 1),
+	APQ8064_RESOURCE(APPS_FABRIC_CFG_CLKMOD, 1),
+	APQ8064_RESOURCE(APPS_FABRIC_CFG_IOCTL, 1),
+	APQ8064_RESOURCE(APPS_FABRIC_ARB, 12),
+
+	APQ8064_RESOURCE(SYS_FABRIC_CFG_HALT, 1),
+	APQ8064_RESOURCE(SYS_FABRIC_CFG_CLKMOD, 1),
+	APQ8064_RESOURCE(SYS_FABRIC_CFG_IOCTL, 1),
+	APQ8064_RESOURCE(SYSTEM_FABRIC_ARB, 30),
+
+	APQ8064_RESOURCE(MMSS_FABRIC_CFG_HALT, 1),
+	APQ8064_RESOURCE(MMSS_FABRIC_CFG_CLKMOD, 1),
+	APQ8064_RESOURCE(MMSS_FABRIC_CFG_IOCTL, 1),
+	APQ8064_RESOURCE(MM_FABRIC_ARB, 21),
+
+	APQ8064_RESOURCE(PM8921_S1, 2),
+	APQ8064_RESOURCE(PM8921_S2, 2),
+	APQ8064_RESOURCE(PM8921_S3, 2),
+	APQ8064_RESOURCE(PM8921_S4, 2),
+	APQ8064_RESOURCE(PM8921_S5, 2),
+	APQ8064_RESOURCE(PM8921_S6, 2),
+	APQ8064_RESOURCE(PM8921_S7, 2),
+	APQ8064_RESOURCE(PM8921_S8, 2),
+
+	APQ8064_RESOURCE(PM8921_L1, 2),
+	APQ8064_RESOURCE(PM8921_L2, 2),
+	APQ8064_RESOURCE(PM8921_L3, 2),
+	APQ8064_RESOURCE(PM8921_L4, 2),
+	APQ8064_RESOURCE(PM8921_L5, 2),
+	APQ8064_RESOURCE(PM8921_L6, 2),
+	APQ8064_RESOURCE(PM8921_L7, 2),
+	APQ8064_RESOURCE(PM8921_L8, 2),
+	APQ8064_RESOURCE(PM8921_L9, 2),
+	APQ8064_RESOURCE(PM8921_L10, 2),
+	APQ8064_RESOURCE(PM8921_L11, 2),
+	APQ8064_RESOURCE(PM8921_L12, 2),
+	APQ8064_RESOURCE(PM8921_L13, 2),
+	APQ8064_RESOURCE(PM8921_L14, 2),
+	APQ8064_RESOURCE(PM8921_L15, 2),
+	APQ8064_RESOURCE(PM8921_L16, 2),
+	APQ8064_RESOURCE(PM8921_L17, 2),
+	APQ8064_RESOURCE(PM8921_L18, 2),
+	APQ8064_RESOURCE(PM8921_L19, 2),
+	APQ8064_RESOURCE(PM8921_L20, 2),
+	APQ8064_RESOURCE(PM8921_L21, 2),
+	APQ8064_RESOURCE(PM8921_L22, 2),
+	APQ8064_RESOURCE(PM8921_L23, 2),
+	APQ8064_RESOURCE(PM8921_L24, 2),
+	APQ8064_RESOURCE(PM8921_L25, 2),
+	APQ8064_RESOURCE(PM8921_L26, 2),
+	APQ8064_RESOURCE(PM8921_L27, 2),
+	APQ8064_RESOURCE(PM8921_L28, 2),
+	APQ8064_RESOURCE(PM8921_L29, 2),
+
+	APQ8064_RESOURCE(PM8921_CLK1, 2),
+	APQ8064_RESOURCE(PM8921_CLK2, 2),
+
+	APQ8064_RESOURCE(PM8921_LVS1, 1),
+	APQ8064_RESOURCE(PM8921_LVS2, 1),
+	APQ8064_RESOURCE(PM8921_LVS3, 1),
+	APQ8064_RESOURCE(PM8921_LVS4, 1),
+	APQ8064_RESOURCE(PM8921_LVS5, 1),
+	APQ8064_RESOURCE(PM8921_LVS6, 1),
+	APQ8064_RESOURCE(PM8921_LVS7, 1),
+
+	APQ8064_RESOURCE(PM8821_S1, 2),
+	APQ8064_RESOURCE(PM8821_S2, 2),
+
+	APQ8064_RESOURCE(PM8821_L1, 2),
+
+	APQ8064_RESOURCE(NCP, 2),
+
+	APQ8064_RESOURCE(CXO_BUFFERS, 1),
+
+	APQ8064_RESOURCE(USB_OTG_SWITCH, 1),
+	APQ8064_RESOURCE(HDMI_SWITCH, 1),
+
+	APQ8064_RESOURCE(DDR_DMM, 2),
+	APQ8064_RESOURCE(VDDMIN_GPIO, 1),
+};
+
+static const struct msm_rpm apq8064_template = {
+	.version = { 999, 0, 0 },
+	.resource_table = apq8064_rpm_resource_table,
+	.nresources = ARRAY_SIZE(apq8064_rpm_resource_table),
 };
 
 /*
- * 8960 data
+ * MSM8960 data
  */
+#define MSM8960_RESOURCE(id, size)		\
+	[MSM_RPM_ ## id] = {			\
+		MSM_RPM_8960_ID_ ## id,		\
+		MSM_RPM_8960_STATUS_ID_ ## id,	\
+		MSM_RPM_8960_SEL_ ## id,	\
+		size				\
+	}
+
 static const struct msm_rpm_resource msm8960_rpm_resource_table[] = {
-	[MSM_RPM_VERSION] = { .ctrl_id = 0, .status_id = 0, .count = 3 },
-	[MSM_RPM_REQ_CTX] = { .ctrl_id = 3, .count = 7 },
-	[MSM_RPM_REQ_SEL] = { .ctrl_id = 11, .count = 3 },
-	[MSM_RPM_ACK_CTX] = { .ctrl_id = 15, .count = 7 },
-	[MSM_RPM_ACK_CTX] = { .ctrl_id = 23, .count = 7 },
-	[MSM_RPM_PM8921_L16] = { .target_id = 163, .status_id = 77, .count = 2, .sel = 53 },
+	MSM8960_RESOURCE(CXO_CLK, 1),
+	MSM8960_RESOURCE(PXO_CLK, 1),
+	MSM8960_RESOURCE(APPS_FABRIC_CLK, 1),
+	MSM8960_RESOURCE(SYSTEM_FABRIC_CLK, 1),
+	MSM8960_RESOURCE(MM_FABRIC_CLK, 1),
+	MSM8960_RESOURCE(DAYTONA_FABRIC_CLK, 1),
+	MSM8960_RESOURCE(SFPB_CLK, 1),
+	MSM8960_RESOURCE(CFPB_CLK, 1),
+	MSM8960_RESOURCE(MMFPB_CLK, 1),
+	MSM8960_RESOURCE(EBI1_CLK, 1),
+
+	MSM8960_RESOURCE(APPS_FABRIC_CFG_HALT, 1),
+	MSM8960_RESOURCE(APPS_FABRIC_CFG_CLKMOD, 1),
+	MSM8960_RESOURCE(APPS_FABRIC_CFG_IOCTL, 1),
+	MSM8960_RESOURCE(APPS_FABRIC_ARB, 12),
+
+	MSM8960_RESOURCE(SYS_FABRIC_CFG_HALT, 1),
+	MSM8960_RESOURCE(SYS_FABRIC_CFG_CLKMOD, 1),
+	MSM8960_RESOURCE(SYS_FABRIC_CFG_IOCTL, 1),
+	MSM8960_RESOURCE(SYSTEM_FABRIC_ARB, 29),
+
+	MSM8960_RESOURCE(MMSS_FABRIC_CFG_HALT, 1),
+	MSM8960_RESOURCE(MMSS_FABRIC_CFG_CLKMOD, 1),
+	MSM8960_RESOURCE(MMSS_FABRIC_CFG_IOCTL, 1),
+	MSM8960_RESOURCE(MM_FABRIC_ARB, 23),
+
+	MSM8960_RESOURCE(PM8921_S1, 2),
+	MSM8960_RESOURCE(PM8921_S2, 2),
+	MSM8960_RESOURCE(PM8921_S3, 2),
+	MSM8960_RESOURCE(PM8921_S4, 2),
+	MSM8960_RESOURCE(PM8921_S5, 2),
+	MSM8960_RESOURCE(PM8921_S6, 2),
+	MSM8960_RESOURCE(PM8921_S7, 2),
+	MSM8960_RESOURCE(PM8921_S8, 2),
+	MSM8960_RESOURCE(PM8921_L1, 2),
+	MSM8960_RESOURCE(PM8921_L2, 2),
+	MSM8960_RESOURCE(PM8921_L3, 2),
+	MSM8960_RESOURCE(PM8921_L4, 2),
+	MSM8960_RESOURCE(PM8921_L5, 2),
+	MSM8960_RESOURCE(PM8921_L6, 2),
+	MSM8960_RESOURCE(PM8921_L7, 2),
+	MSM8960_RESOURCE(PM8921_L8, 2),
+	MSM8960_RESOURCE(PM8921_L9, 2),
+	MSM8960_RESOURCE(PM8921_L10, 2),
+	MSM8960_RESOURCE(PM8921_L11, 2),
+	MSM8960_RESOURCE(PM8921_L12, 2),
+	MSM8960_RESOURCE(PM8921_L13, 2),
+	MSM8960_RESOURCE(PM8921_L14, 2),
+	MSM8960_RESOURCE(PM8921_L15, 2),
+	MSM8960_RESOURCE(PM8921_L16, 2),
+	MSM8960_RESOURCE(PM8921_L17, 2),
+	MSM8960_RESOURCE(PM8921_L18, 2),
+	MSM8960_RESOURCE(PM8921_L19, 2),
+	MSM8960_RESOURCE(PM8921_L20, 2),
+	MSM8960_RESOURCE(PM8921_L21, 2),
+	MSM8960_RESOURCE(PM8921_L22, 2),
+	MSM8960_RESOURCE(PM8921_L23, 2),
+	MSM8960_RESOURCE(PM8921_L24, 2),
+	MSM8960_RESOURCE(PM8921_L25, 2),
+	MSM8960_RESOURCE(PM8921_L26, 2),
+	MSM8960_RESOURCE(PM8921_L27, 2),
+	MSM8960_RESOURCE(PM8921_L28, 2),
+	MSM8960_RESOURCE(PM8921_L29, 2),
+	MSM8960_RESOURCE(PM8921_CLK1, 2),
+	MSM8960_RESOURCE(PM8921_CLK2, 2),
+	MSM8960_RESOURCE(PM8921_LVS1, 1),
+	MSM8960_RESOURCE(PM8921_LVS2, 1),
+	MSM8960_RESOURCE(PM8921_LVS3, 1),
+	MSM8960_RESOURCE(PM8921_LVS4, 1),
+	MSM8960_RESOURCE(PM8921_LVS5, 1),
+	MSM8960_RESOURCE(PM8921_LVS6, 1),
+	MSM8960_RESOURCE(PM8921_LVS7, 1),
+
+	MSM8960_RESOURCE(NCP, 2),
+	MSM8960_RESOURCE(CXO_BUFFERS, 1),
+	MSM8960_RESOURCE(USB_OTG_SWITCH, 1),
+	MSM8960_RESOURCE(HDMI_SWITCH, 1),
+	MSM8960_RESOURCE(DDR_DMM, 2),
 };
 
 static const struct msm_rpm msm8960_template = {
@@ -79,40 +277,16 @@ static const struct msm_rpm msm8960_template = {
 };
 
 static const struct of_device_id msm_rpm_of_match[] = {
+	{ .compatible = "qcom,apq8064-rpm", .data = &apq8064_template },
 	{ .compatible = "qcom,msm8960-rpm", .data = &msm8960_template },
 };
 MODULE_DEVICE_TABLE(of, msm_rpm_of_match);
 
-/*
- * Implementation
- */
-int msm_rpm_read_status(const struct device *dev, int resource, u32 *buf, size_t count)
+int msm_rpm_write(const struct device *dev, enum msm_rpm_resource_id resource, u32 *buf, size_t size)
 {
 	const struct msm_rpm_resource *res;
 	struct msm_rpm *rpm = dev_get_drvdata(dev);
-	unsigned i;
-
-	if (WARN_ON(resource < 0 || resource >= rpm->nresources))
-		return -EINVAL;
-
-	res = &rpm->resource_table[resource];
-	if (WARN_ON(res->count != count))
-		return -EINVAL;
-
-	for (i = 0; i < count; i++)
-		buf[i] = readl(MSM_STATUS_REG(rpm, res->status_id + i));
-
-	return 0;
-}
-EXPORT_SYMBOL(msm_rpm_read_status);
-
-int msm_rpm_write(const struct device *dev, int resource, u32 *buf, size_t count)
-{
-	const struct msm_rpm_resource *req_ctx;
-	const struct msm_rpm_resource *sel_map;
-	const struct msm_rpm_resource *res;
-	struct msm_rpm *rpm = dev_get_drvdata(dev);
-	unsigned long sel_mask[3] = { 0 };
+	unsigned long sel_mask[MSM_RPM_REQ_SEL_COUNT] = { 0 };
 	int ret = 0;
 	int i;
 
@@ -120,22 +294,20 @@ int msm_rpm_write(const struct device *dev, int resource, u32 *buf, size_t count
 		return -EINVAL;
 
 	res = &rpm->resource_table[resource];
-	if (WARN_ON(res->count != count))
+	if (WARN_ON(res->size != size))
 		return -EINVAL;
 
 	mutex_lock(&rpm->lock);
 
-	for (i = 0; i < res->count; i++)
+	for (i = 0; i < res->size; i++)
 		writel(buf[i], MSM_REQ_REG(rpm, res->target_id + i));
 
-	bitmap_set(sel_mask, res->sel, 1);
-	sel_map = &rpm->resource_table[MSM_RPM_REQ_SEL];
-	for (i = 0; i < sel_map->count; i++)
-		writel(sel_mask[i], MSM_CTRL_REG(rpm, sel_map->ctrl_id + i));
+	bitmap_set(sel_mask, res->select_id, 1);
+	for (i = 0; i < ARRAY_SIZE(sel_mask); i++)
+		writel(sel_mask[i], MSM_CTRL_REG(rpm, MSM_RPM_REQ_SEL + i));
 
 	/* BIT(0) is active mode */
-	req_ctx = &rpm->resource_table[MSM_RPM_REQ_CTX];
-	writel(BIT(0), MSM_CTRL_REG(rpm, req_ctx->ctrl_id));
+	writel(BIT(0), MSM_CTRL_REG(rpm, MSM_RPM_REQ_CTX));
 
 	reinit_completion(&rpm->ack);
 
@@ -155,23 +327,17 @@ EXPORT_SYMBOL(msm_rpm_write);
 
 static irqreturn_t msm_rpm_ack_interrupt(int irq, void *dev)
 {
-	const struct msm_rpm_resource *ack_ctx;
-	const struct msm_rpm_resource *ack_sel;
 	struct msm_rpm *rpm = dev;
-	unsigned long sel[3];
+	unsigned long sel[MSM_RPM_ACK_SEL_COUNT];
 	unsigned long ack;
 	int i;
 
-	ack_ctx = &rpm->resource_table[MSM_RPM_ACK_CTX];
-	ack_sel = &rpm->resource_table[MSM_RPM_ACK_SEL];
-
-	ack = readl(MSM_CTRL_REG(rpm, ack_ctx->ctrl_id));
-	for (i = 0; i < ack_sel->count; i++)
-		sel[i] = readl(MSM_CTRL_REG(rpm, ack_sel->ctrl_id + i));
-
-	for (i = 0; i < ack_sel->count; i++)
-		writel(0, MSM_CTRL_REG(rpm, ack_sel->ctrl_id + i));
-	writel(0, MSM_CTRL_REG(rpm, ack_ctx->ctrl_id));
+	ack = readl(MSM_CTRL_REG(rpm, MSM_RPM_ACK_CTX));
+	for (i = 0; i < MSM_RPM_ACK_SEL_COUNT; i++) {
+		sel[i] = readl(MSM_CTRL_REG(rpm, MSM_RPM_ACK_SEL + i));
+		writel(0, MSM_CTRL_REG(rpm, MSM_RPM_ACK_SEL + i));
+	}
+	writel(0, MSM_CTRL_REG(rpm, MSM_RPM_ACK_CTX));
 
 	/* BIT(30) is notification */
 	if (ack & BIT(30)) {
@@ -202,14 +368,12 @@ static irqreturn_t msm_rpm_wakeup_interrupt(int irq, void *dev)
 
 static int msm_rpm_probe(struct platform_device *pdev)
 {
-	const struct msm_rpm_resource *version_ctrl;
 	const struct of_device_id *match;
 	const struct msm_rpm *template;
 	struct resource *res;
 	struct msm_rpm *rpm;
 	u32 fw_version[3];
 	int ret;
-	int i;
 
 	dev_dbg(&pdev->dev, "================ msm_rpm_probe ================\n");
 
@@ -261,7 +425,9 @@ static int msm_rpm_probe(struct platform_device *pdev)
 
 	dev_set_drvdata(&pdev->dev, rpm);
 
-	ret = msm_rpm_read_status(&pdev->dev, MSM_RPM_VERSION, fw_version, 3);
+	fw_version[0] = readl(MSM_STATUS_REG(rpm, MSM_RPM_VERSION + 0));
+	fw_version[1] = readl(MSM_STATUS_REG(rpm, MSM_RPM_VERSION + 1));
+	fw_version[2] = readl(MSM_STATUS_REG(rpm, MSM_RPM_VERSION + 2));
 	if (fw_version[0] != rpm->version[0]) {
 		dev_err(&pdev->dev, "RPM version %u.%u.%u incompatible with "
 				    "this driver version %u.%u.%u\n",
@@ -278,9 +444,9 @@ static int msm_rpm_probe(struct platform_device *pdev)
 							fw_version[1],
 							fw_version[2]);
 
-	version_ctrl = &rpm->resource_table[MSM_RPM_VERSION];
-	for (i = 0; i < version_ctrl->count; i++)
-		writel(fw_version[i], MSM_CTRL_REG(rpm, version_ctrl->ctrl_id + i));
+	writel(fw_version[0], MSM_CTRL_REG(rpm, MSM_RPM_VERSION + 0));
+	writel(fw_version[1], MSM_CTRL_REG(rpm, MSM_RPM_VERSION + 1));
+	writel(fw_version[2], MSM_CTRL_REG(rpm, MSM_RPM_VERSION + 2));
 
 	ret = devm_request_irq(&pdev->dev,
 			       rpm->irq_ack,
